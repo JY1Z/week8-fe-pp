58721f72a019274a272227a0dbff2c1b2cd2861e
jiayuez <Jiayue.Zheng@metropolia.fi>
2024-10-07 11:15:53 +0300

    Add Part 3

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,9 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import ThemeProvider from "./context/ThemeProvider";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AuthProvider from "./context/AuthProvider";
-// import RouteGuard from "./components/RouteGuard";
+import ThemeProvider from "./context/ThemeProvider";
+import RouteGuard from "./components/RouteGuard";
 
-// pages & components
+// Pages & Components
 import Navbar from "./components/Navbar";
 import Home from "./pages/HomePage";
 import AddJobPage from "./pages/AddJobPage";
@@ -15,8 +15,8 @@ import Signup from "./pages/Signup";
 
 const App = () => {
   return (
-    <AuthProvider>
-      <ThemeProvider>
+    <ThemeProvider>
+      <AuthProvider>
         <BrowserRouter>
           <div className="App">
             <Navbar />
@@ -24,18 +24,88 @@ const App = () => {
               <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/jobs/:id" element={<JobPage />} />
-                <Route path="/jobs/add-job" element={<AddJobPage />} />
-                <Route path="/edit-job/:id" element={<EditJobPage />} />
-                <Route path="/signup" element={<Signup />} />
-                <Route path="/login" element={<Login />} />
+                <Route
+                  path="/jobs/add-job"
+                  element={
+                    <RouteGuard requireAuth={true}>
+                      <AddJobPage />
+                    </RouteGuard>
+                  }
+                />
+                <Route
+                  path="/edit-job/:id"
+                  element={
+                    <RouteGuard requireAuth={true}>
+                      <EditJobPage />
+                    </RouteGuard>
+                  }
+                />
+                <Route
+                  path="/signup"
+                  element={
+                    <RouteGuard requireAuth={false}>
+                      <Signup />
+                    </RouteGuard>
+                  }
+                />
+                <Route
+                  path="/login"
+                  element={
+                    <RouteGuard requireAuth={false}>
+                      <Login />
+                    </RouteGuard>
+                  }
+                />
                 <Route path="*" element={<NotFoundPage />} />
               </Routes>
             </div>
           </div>
         </BrowserRouter>
-      </ThemeProvider>
-    </AuthProvider>
+      </AuthProvider>
+    </ThemeProvider>
   );
 };
 
 export default App;
+
+// import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+// import ThemeProvider from "./context/ThemeProvider";
+// import AuthProvider from "./context/AuthProvider";
+// // import RouteGuard from "./components/RouteGuard";
+
+// // pages & components
+// import Navbar from "./components/Navbar";
+// import Home from "./pages/HomePage";
+// import AddJobPage from "./pages/AddJobPage";
+// import JobPage from "./pages/JobPage";
+// import EditJobPage from "./pages/EditJobPage";
+// import NotFoundPage from "./pages/NotFoundPage";
+// import Login from "./pages/Login";
+// import Signup from "./pages/Signup";
+
+// const App = () => {
+//   return (
+//     <AuthProvider>
+//       <ThemeProvider>
+//         <BrowserRouter>
+//           <div className="App">
+//             <Navbar />
+//             <div className="content">
+//               <Routes>
+//                 <Route path="/" element={<Home />} />
+//                 <Route path="/jobs/:id" element={<JobPage />} />
+//                 <Route path="/jobs/add-job" element={<AddJobPage />} />
+//                 <Route path="/edit-job/:id" element={<EditJobPage />} />
+//                 <Route path="/signup" element={<Signup />} />
+//                 <Route path="/login" element={<Login />} />
+//                 <Route path="*" element={<NotFoundPage />} />
+//               </Routes>
+//             </div>
+//           </div>
+//         </BrowserRouter>
+//       </ThemeProvider>
+//     </AuthProvider>
+//   );
+// };
+
+// export default App;
